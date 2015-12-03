b08dd22722397bdce7fcf8caf062677e9651b901
MozaAl-hemaidi <ma1001810@qu.edu.qa>
2015-12-03 09:17:46 +0300

    testing the github

--- GainMoneyApp/app/src/main/java/qa/edu/qu/cmps/gainmoneyapp/MainActivity.java
+++ GainMoneyApp/app/src/main/java/qa/edu/qu/cmps/gainmoneyapp/MainActivity.java
@@ -2,6 +2,7 @@ package qa.edu.qu.cmps.gainmoneyapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("MOZA", "HELLO main");
+        Log.i("Whay","You are not change");
+        
     }
+
 }
