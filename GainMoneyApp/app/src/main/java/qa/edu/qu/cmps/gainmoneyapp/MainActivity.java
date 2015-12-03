0c69fce0b801b8fdc2b8609e97c02d2abc17852d
MozaAl-hemaidi <ma1001810@qu.edu.qa>
2015-12-03 09:19:55 +0300

    testing
    am just test if i can use this tool

--- GainMoneyApp/app/src/main/java/qa/edu/qu/cmps/gainmoneyapp/MainActivity.java
+++ GainMoneyApp/app/src/main/java/qa/edu/qu/cmps/gainmoneyapp/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i("MOZA", "HELLO main");
         Log.i("Whay","You are not change");
-        
+
     }
 
 }
